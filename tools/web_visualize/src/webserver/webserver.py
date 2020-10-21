f1fcaa1f54a2b8a8704d49fe636e56246addb553
zhupengdong <zhupengdong@cambricon.com>
2020-10-21 10:38:46 +0800

    add license

--- tools/web_visualize/src/webserver/webserver.py
+++ tools/web_visualize/src/webserver/webserver.py
@@ -1,5 +1,37 @@
-from webserver.core import *
+###############################################################################
+# Copyright (C) [2020] by Cambricon, Inc. All rights reserved
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+###############################################################################
+
+import os
+import time
+import json
+import signal
+import logging
+import subprocess
+
 from webserver import core
+from webserver.core import json_path
+from webserver.core import app, render_template, Response, request
+
+from sys import path
+path.append("webserver")
+import pycnservice
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 logging.info('[CNStreamService] webserver path: ' + basedir)
