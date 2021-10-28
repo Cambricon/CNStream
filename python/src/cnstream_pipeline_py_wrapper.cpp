cd6e21033faffd27f8f155672ebe272d0164d0da
zhupengdong <zhupengdong@cambricon.com>
2021-10-28 15:32:18 +0800

    add the sample of receiving data in App

--- python/src/cnstream_pipeline_py_wrapper.cpp
+++ python/src/cnstream_pipeline_py_wrapper.cpp
@@ -18,6 +18,7 @@
  * THE SOFTWARE.
  *************************************************************************/
 
+#include <pybind11/functional.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
